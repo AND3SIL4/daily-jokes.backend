74de88e5dcbab625fc05f5656f96dfa4eb27e48c
Andres Silva <Andres.silva@globalteksecurity.com>
2025-03-21 18:41:15 -0500

    fix(app): cors and improve the response giving a date

--- app/main.py
+++ app/main.py
@@ -2,12 +2,22 @@ import os
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 from app.api import jokes
 
 load_dotenv()
 app = FastAPI(title="Daily Jokes API REST")
+# Allow cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Constants
 VERSION = os.getenv("VERSION")
